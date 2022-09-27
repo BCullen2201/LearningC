2e04fb6ddda4c6a637cb98537e72c1b022d18935
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-27 17:16:29 -0400

    added game.c, created mad libs game

--- game.c
+++ game.c
@@ -2,7 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-	printf("Hello, World!\n");
+	// Mad Libs style game
+	char color[20];
+	char pluralNoun[20];
+	char adjective[20];
+	char pluralNoun2[20];
+	
+	printf("Enter a color: ");
+	scanf("%s", color);
+	printf("Enter a plural noun: ");
+	scanf("%s", pluralNoun);
+	printf("Enter a adjective: ");
+	scanf("%s", adjective);
+	printf("Enter a plural noun: ");
+	scanf("%s", pluralNoun2);
+	
+	printf("Roses are %s,\n", color);
+	printf("%s are %s,\n", pluralNoun, color);
+	printf("I love %s %s\n", adjective, pluralNoun2);
 	
 	return 0;
 }
