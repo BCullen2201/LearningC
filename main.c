08fb385054a0ce0c78d61a2b71e704ded15a700b
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-27 15:20:21 -0400

    made basic calculator, added comments to organize

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int main() {
+	// Getting user input
 	char name[20];
 	printf("What is your name?: ");
 	fgets(name, 20, stdin);
@@ -11,6 +12,17 @@ int main() {
 	printf("How old are you?: ");
 	scanf("%d", &age);
 	printf("You are %d years old.\n", age);
+	
+	// Very basic calculator
+	int num1;
+	int num2;
+	
+	printf("Enter first number: ");
+	scanf("%d", &num1); // I'm not sure what a pointer is yet, but I guess I need them to use scanf to store ints in a variable?
+	printf("Enter second number: ");
+	scanf("%d", &num2);
+	
+	printf("Answer: %d\n", num1 + num2);
 	
 	return 0;
 }
