daa6487c4219e123ff54cf462ff07dbbd03aa823
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-28 13:36:23 -0400

    Created array, print 6th element

--- array.c
+++ array.c
@@ -3,9 +3,8 @@
 
 int main() {
 	int luckyNumbers[] = {4, 8, 15, 16, 24, 42};
-	luckyNumbers[5] = 200;
-	
-	printf("%d\n", luckyNumbers[5]); // Learned that trying to append an element to the array causes an overflow
-	
+	luckyNumbers[5] = 200; // Learned that trying to append an element to the array causes an overflow
+	printf("%d\n", luckyNumbers[5]);
+
 	return 0;
 }
