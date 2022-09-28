43b75bb8a24a6623ea66b8929ed9e4183a1e3138
Brandon Cullen <brandoncullen2201@gmail.com>
2022-09-28 13:08:59 -0400

    Get user name and display at end

--- game.c
+++ game.c
@@ -7,6 +7,8 @@ int main() {
 	char pluralNoun[20];
 	char adjective[20];
 	char pluralNoun2[20];
+	char firstName[20];
+	char lastName[20];
 	
 	printf("Enter a color: ");
 	scanf("%s", color);
@@ -16,10 +18,13 @@ int main() {
 	scanf("%s", adjective);
 	printf("Enter a plural noun: ");
 	scanf("%s", pluralNoun2);
+	printf("Enter your first and last name: ");
+	scanf("%s%s", firstName, lastName);
 	
 	printf("Roses are %s,\n", color);
 	printf("%s are %s,\n", pluralNoun, color);
 	printf("I love %s %s\n", adjective, pluralNoun2);
+	printf("A poem by %s %s\n", firstName, lastName);
 	
 	return 0;
 }
